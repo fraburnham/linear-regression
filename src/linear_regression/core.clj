ddf497e81664d8a3580fa578b34becdd0c34741d
Frank Burnham <fra.burnham@gmail.com>
2014-12-05 20:02:17 -0500

    Made the core loops more general and added logistic regression

--- src/linear_regression/core.clj
+++ src/linear_regression/core.clj
@@ -6,51 +6,24 @@
 (defn squared-diff [x y]
   (square (- x y)))
 
-;;----------------UNIVAR SPECIFIC FUNCTIONS----------------
-(defn univar-hypothesis [theta0 theta1 x]
-  (+ (* theta1 x) theta0))
-
-(defn univar-costfn [hypo-y actual-y]
-  (let [m (/ 1 (* 2 (count hypo-y)))]
-    (* m (reduce + (map squared-diff hypo-y actual-y)))))
-
-;partial derivative of costfn with respect to theta0
-(defn univar-dtheta0 [hypo-y actual-y]
-  (let [m (count hypo-y)]
-    (* (/ 1 m) (reduce + (map - hypo-y actual-y)))))
-
-;partial derivative of costfn with respect to theta1
-(defn univar-dtheta1 [hypo-y actual-y]
-  (let [m (count hypo-y)]
-    (* (/ 1 m) (reduce + (map * (map - hypo-y actual-y) hypo-y)))))
-
-(defn univar-batch-gradient-descent [thetas alpha hypo-y actual-y]
-  (let [[theta0 theta1] thetas]
-    [(- theta0 (* alpha (univar-dtheta0 hypo-y actual-y)))
-     (- theta1 (* alpha (univar-dtheta1 hypo-y actual-y)))]))
-
-(defn univar-linear-regression [alpha thetas training-inputs training-outputs]
-  (loop [thetas thetas]
-    (let [hypo-ys (map (partial univar-hypothesis (first thetas) (last thetas))
-                       training-inputs)
-          newthetas (univar-batch-gradient-descent thetas alpha hypo-ys
-                                            training-outputs)]
-      (if (or (Double/isNaN (first thetas))
-              (= thetas newthetas))
-        thetas
-        (recur newthetas)))))
-
-;Added in some changes for testing
-;transients and loop / recur instead of map reduce
-;;----------------MULTI/UNIVAR FUNCTIONS----------------
 ;hypothesis requires the first feature to be 1 always
-(defn hypothesis [thetas features]
+(defn linear-hypothesis [thetas features]
   (reduce + (map * thetas features)))
 
-;gonna try to get some speed out of costfn...
-(defn costfn [hypo-ys actual-ys]
+(defn linear-costfn [hypo-ys actual-ys]
   (let [m (count hypo-ys)]
     (/ 1 (* 2 m) (reduce + (map squared-diff hypo-ys actual-ys)))))
+
+;hypothesis requires the first feature to be 1 always
+(defn logistic-hypothesis [thetas features]
+  (/ 1 (+ 1 (* (Math/E) (- (reduce + (map * thetas features)))))))
+
+(defn logistic-costfn [hypo-ys actual-ys]
+  (map (fn [hypo-y actual-y]
+         (if (= actual-y 1)
+           (- (Math/log hypo-y))
+           (- (Math/log (- 1 hypo-y)))))
+       hypo-ys actual-ys))
 
 ;thetas ((t1 t2 t3) (t1 t2 t3))
 ;features ((f1 f2 f3) (f1 f2 f3))
@@ -66,14 +39,14 @@
 ;((1 feature feature feature) (1 feature feature feature))
 ;thetas
 ;(theta0 theta1 theta2)
-(defn linear-regression [alpha training-inputs training-outputs]
+(defn regression [hypothesis-fn cost-fn alpha training-inputs training-outputs]
   (loop [thetas
          (cons 1 (repeatedly (dec (count (first training-inputs))) (constantly 0)))
          cost 0]
-    (let [hypo-ys (map (partial hypothesis thetas) training-inputs)
+    (let [hypo-ys (map (partial hypothesis-fn thetas) training-inputs)
           new-thetas (batch-gradient-descent thetas alpha
-                                            training-inputs hypo-ys training-outputs)
-          new-cost (costfn hypo-ys training-outputs)]
+                                             training-inputs hypo-ys training-outputs)
+          new-cost (cost-fn hypo-ys training-outputs)]
       (if (or (> cost new-cost)
               (= thetas new-thetas))
         thetas
